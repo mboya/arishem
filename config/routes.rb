c26266522c6fd5d14905fa6752dd0a70659a13b3
mboya <mboyaberry@gmail.com>
2023-09-19 09:12:55 +0300

    controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   use_doorkeeper
   jsonapi_resources :users, only: %i[index show create]
+
+  post 'pesa/validate'
+  post 'pesa/confirm'
+  post 'pesa/queue_timeout'
+  post 'pesa/result'
+  post 'pesa/call_back'
+  get 'pesa/pesa_token'
 end
