ac05bf0a98a281e89f749cd96aff626e2b4df432
mboya <mboyaberry@gmail.com>
2023-09-21 22:18:44 +0300

    Update pesa_controller.rb
    This is going to receive feedback from provider on incoming payments.

--- app/controllers/pesa_controller.rb
+++ app/controllers/pesa_controller.rb
@@ -2,6 +2,7 @@
 
 class PesaController < ApplicationController
   skip_before_action :doorkeeper_authorize!
+
   def validate
     render json: { success: :ok }
   end
@@ -19,11 +20,44 @@ class PesaController < ApplicationController
   end
 
   def call_back
-    render json: { success: :ok, params: }
+    amount, phonenumber = nil
+
+    return unless params[:Body][:stkCallback][:CallbackMetadata].present?
+
+    params[:Body][:stkCallback][:CallbackMetadata][:Item].each do |item|
+      case item[:Name].downcase
+      when 'amount'
+        amount = item[:Value]
+      when 'phonenumber'
+        phonenumber = item[:Value]
+      end
+    end
+
+    wallet_id = find_wallet(phonenumber)
+    credit_wallet_txn(wallet_id, amount, phonenumber, params)
+
+    render json: 'received'
   end
 
   def pesa_token
     token = Stk::AccessToken.call(ENV['key'], ENV['secret'])
     render json: { token: token }
+  end
+
+  private
+
+  def find_wallet(phonenumber)
+    User.find_by(phone: phonenumber).wallet.id
+  end
+
+  def credit_wallet_txn(wallet_id, amount, phonenumber, params)
+    WalletTransaction.create({
+                               wallet_id: wallet_id,
+                               amount_in_cents: (amount.to_i * 100),
+                               txn_type: 'credit',
+                               phone: phonenumber,
+                               result: params
+                             })
+    CreditJob.perform_now(wallet.id)
   end
 end
