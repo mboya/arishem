0dee0e6b508c021508e7f453cb6fd24d41673b57
mboya <mboyaberry@gmail.com>
2023-10-06 13:02:02 +0300

    Update user_resource.rb
    Control on what can be updated.

--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -9,6 +9,10 @@ class UserResource < JSONAPI::Resource
     super - %i[first_name last_name status country_code email password]
   end
 
+  def self.updatable_fields(context)
+    super - %i[phone country_code]
+  end
+
   def names
     "#{@model.last_name}, #{@model.first_name}"
   end
